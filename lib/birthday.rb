0afa3ffe1ae969f2cfeb043451da93332a8fb349
Reina Ewing <reina.castro3@gmail.com>
2019-05-21 16:01:14 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,6 +13,10 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids_kids.each do |kids_name, age|
-    if 
+    if age < 12 
+      puts puts "Happy Birthday #{kids_name}! You are now #{age} years old!
+    end 
+  end 
+end 
 
 
